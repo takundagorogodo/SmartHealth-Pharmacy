752ec9d606ced4bd4bfb5efdc8f56d8899a8e2b8
GOROGODO TAKUNDA LEONARD <gorogodotakundah26@gmail.com>
2026-04-14 15:36:54 +0530

    Add files via upload

--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -1,6 +1,11 @@
 import express from "express";
+import consultationRoutes from "./consultationRoutes.js";
+import healthRecordRoutes from "./healthRecordRoutes.js";
 
 
 const router = express.Router();
+
+router.use("/consultations",consultationRoutes);
+router.use("/health-record",healthRecordRoutes);
 
 export default router;
